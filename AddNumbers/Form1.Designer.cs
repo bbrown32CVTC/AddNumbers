48dc4d8208c4eeb47c09a09b127d5ff876631bd0
Bruce Brown <bbrown32@student.cvtc.edu>
2024-07-06 12:12:44 -0500

    Updated app to only use one input, allow enter key to add input to sum, accept input value 0 as a flag to output running total, display the total sum of 0 when app starts, and changed the Restart button text to "clear / reset". Enhanced the form design.

--- AddNumbers/Form1.Designer.cs
+++ AddNumbers/Form1.Designer.cs
@@ -29,173 +29,102 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            firstValueLabel = new Label();
-            secondValueLabel = new Label();
-            firstValueText = new TextBox();
-            secondValueText = new TextBox();
-            button1 = new Button();
-            resultTextBox = new TextBox();
+            inputValueLabel = new Label();
+            inputValueTB = new TextBox();
+            addBtn = new Button();
+            totalSumTB = new TextBox();
             label4 = new Label();
-            addMoreLabel = new Label();
-            addMoreTB = new TextBox();
-            button2 = new Button();
-            initialValuesGB = new GroupBox();
-            addMoreGB = new GroupBox();
-            initialValuesGB.SuspendLayout();
-            addMoreGB.SuspendLayout();
+            resetBtn = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(45, 9);
+            label1.Font = new Font("Verdana", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(22, 9);
             label1.Name = "label1";
-            label1.Size = new Size(349, 29);
+            label1.Size = new Size(427, 35);
             label1.TabIndex = 0;
             label1.Text = "Simple App to Add Numbers";
             // 
-            // firstValueLabel
+            // inputValueLabel
             // 
-            firstValueLabel.AutoSize = true;
-            firstValueLabel.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            firstValueLabel.Location = new Point(6, 19);
-            firstValueLabel.Name = "firstValueLabel";
-            firstValueLabel.Size = new Size(111, 23);
-            firstValueLabel.TabIndex = 1;
-            firstValueLabel.Text = "First Value";
+            inputValueLabel.AutoSize = true;
+            inputValueLabel.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            inputValueLabel.Location = new Point(45, 93);
+            inputValueLabel.Name = "inputValueLabel";
+            inputValueLabel.Size = new Size(149, 29);
+            inputValueLabel.TabIndex = 1;
+            inputValueLabel.Text = "Input Value";
             // 
-            // secondValueLabel
+            // inputValueTB
             // 
-            secondValueLabel.AutoSize = true;
-            secondValueLabel.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            secondValueLabel.Location = new Point(6, 73);
-            secondValueLabel.Name = "secondValueLabel";
-            secondValueLabel.Size = new Size(139, 23);
-            secondValueLabel.TabIndex = 2;
-            secondValueLabel.Text = "Second Value";
+            inputValueTB.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            inputValueTB.Location = new Point(214, 95);
+            inputValueTB.Name = "inputValueTB";
+            inputValueTB.Size = new Size(215, 27);
+            inputValueTB.TabIndex = 3;
             // 
-            // firstValueText
+            // addBtn
             // 
-            firstValueText.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            firstValueText.Location = new Point(163, 19);
-            firstValueText.Name = "firstValueText";
-            firstValueText.Size = new Size(215, 27);
-            firstValueText.TabIndex = 3;
+            addBtn.BackColor = Color.LightSkyBlue;
+            addBtn.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            addBtn.Location = new Point(45, 159);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(97, 57);
+            addBtn.TabIndex = 5;
+            addBtn.Text = "Add";
+            addBtn.UseVisualStyleBackColor = false;
+            addBtn.Click += button1_Click;
             // 
-            // secondValueText
+            // totalSumTB
             // 
-            secondValueText.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            secondValueText.Location = new Point(163, 73);
-            secondValueText.Name = "secondValueText";
-            secondValueText.Size = new Size(215, 27);
-            secondValueText.TabIndex = 4;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.LightSkyBlue;
-            button1.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(45, 265);
-            button1.Name = "button1";
-            button1.Size = new Size(116, 67);
-            button1.TabIndex = 5;
-            button1.Text = "Add";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // resultTextBox
-            // 
-            resultTextBox.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            resultTextBox.Location = new Point(179, 288);
-            resultTextBox.Name = "resultTextBox";
-            resultTextBox.Size = new Size(116, 27);
-            resultTextBox.TabIndex = 6;
+            totalSumTB.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            totalSumTB.Location = new Point(214, 189);
+            totalSumTB.Name = "totalSumTB";
+            totalSumTB.Size = new Size(215, 27);
+            totalSumTB.TabIndex = 6;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(198, 265);
+            label4.Location = new Point(264, 159);
             label4.Name = "label4";
-            label4.Size = new Size(67, 18);
+            label4.Size = new Size(89, 18);
             label4.TabIndex = 7;
-            label4.Text = "Answer";
+            label4.Text = "Total Sum";
             // 
-            // addMoreLabel
+            // resetBtn
             // 
-            addMoreLabel.AutoSize = true;
-            addMoreLabel.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            addMoreLabel.Location = new Point(6, 28);
-            addMoreLabel.Name = "addMoreLabel";
-            addMoreLabel.Size = new Size(112, 23);
-            addMoreLabel.TabIndex = 8;
-            addMoreLabel.Text = "Next Value";
-            // 
-            // addMoreTB
-            // 
-            addMoreTB.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            addMoreTB.Location = new Point(163, 24);
-            addMoreTB.Name = "addMoreTB";
-            addMoreTB.Size = new Size(215, 27);
-            addMoreTB.TabIndex = 9;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Red;
-            button2.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(313, 265);
-            button2.Name = "button2";
-            button2.Size = new Size(116, 67);
-            button2.TabIndex = 10;
-            button2.Text = "Restart";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
-            // 
-            // initialValuesGB
-            // 
-            initialValuesGB.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            initialValuesGB.Controls.Add(firstValueLabel);
-            initialValuesGB.Controls.Add(secondValueLabel);
-            initialValuesGB.Controls.Add(firstValueText);
-            initialValuesGB.Controls.Add(secondValueText);
-            initialValuesGB.Location = new Point(45, 65);
-            initialValuesGB.Name = "initialValuesGB";
-            initialValuesGB.Size = new Size(384, 107);
-            initialValuesGB.TabIndex = 11;
-            initialValuesGB.TabStop = false;
-            // 
-            // addMoreGB
-            // 
-            addMoreGB.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            addMoreGB.Controls.Add(addMoreLabel);
-            addMoreGB.Controls.Add(addMoreTB);
-            addMoreGB.Location = new Point(45, 171);
-            addMoreGB.Name = "addMoreGB";
-            addMoreGB.Size = new Size(384, 76);
-            addMoreGB.TabIndex = 12;
-            addMoreGB.TabStop = false;
+            resetBtn.BackColor = Color.Red;
+            resetBtn.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            resetBtn.ForeColor = SystemColors.ButtonHighlight;
+            resetBtn.Location = new Point(45, 242);
+            resetBtn.Name = "resetBtn";
+            resetBtn.Size = new Size(384, 57);
+            resetBtn.TabIndex = 10;
+            resetBtn.Text = "Clear / Reset";
+            resetBtn.UseVisualStyleBackColor = false;
+            resetBtn.Click += button2_Click;
             // 
             // Form1
             // 
+            AcceptButton = addBtn;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(478, 344);
-            Controls.Add(button2);
-            Controls.Add(addMoreGB);
-            Controls.Add(initialValuesGB);
+            ClientSize = new Size(478, 311);
+            Controls.Add(inputValueTB);
+            Controls.Add(inputValueLabel);
+            Controls.Add(resetBtn);
             Controls.Add(label4);
-            Controls.Add(resultTextBox);
-            Controls.Add(button1);
+            Controls.Add(totalSumTB);
+            Controls.Add(addBtn);
             Controls.Add(label1);
             Name = "Form1";
             SizeGripStyle = SizeGripStyle.Hide;
-            Text = "AddNums";
-            initialValuesGB.ResumeLayout(false);
-            initialValuesGB.PerformLayout();
-            addMoreGB.ResumeLayout(false);
-            addMoreGB.PerformLayout();
+            Text = "Accumulator";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -203,17 +132,11 @@
         #endregion
 
         private Label label1;
-        private Label firstValueLabel;
-        private Label secondValueLabel;
-        private TextBox firstValueText;
-        private TextBox secondValueText;
-        private Button button1;
-        private TextBox resultTextBox;
+        private Label inputValueLabel;
+        private TextBox inputValueTB;
+        private Button addBtn;
+        private TextBox totalSumTB;
         private Label label4;
-        private Label addMoreLabel;
-        private TextBox addMoreTB;
-        private Button button2;
-        private GroupBox initialValuesGB;
-        private GroupBox addMoreGB;
+        private Button resetBtn;
     }
 }
--- AddNumbers/Form1.cs
+++ AddNumbers/Form1.cs
@@ -2,105 +2,105 @@ using System.Diagnostics.Metrics;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
+/*
+ * Created by: Bruce Brown 
+ * Date Created: 6/26/2024
+ * 
+ * 7/6/2024 - Updated app to only use one input, allow enter key to add input to sum, 
+ * accept input value 0 as a flag to output running total, display the total sum of 0 
+ * when app starts, and changed the Restart button text to "clear / reset". Enhanced the 
+ * form design.
+ * 
+ */
+
 namespace AddNumbers
 {
     /// <summary>
     /// Simple Win Form App to demonstrate a simple application
-    /// that can add two numbers and display the output.
+    /// that can keep a running total and display the output.
     /// Will catch errors when the input is not a number
-    /// and output the error message.The client can utilize
+    /// and output the error message.The user can utilize
     /// the running total calculation option after the first
-    /// calculation. Hitting the restart button will refresh
+    /// calculation. Inputting 0 will output the running total
+    /// and refresh the application to its original state.
+    /// Hitting the clear/reset button will also refresh
     /// the application to its original state.
     /// </summary>
+
 
     public partial class Form1 : Form
     {
         // Initialize variables
-        private double value1, value2, value3, answer, runningTotal;
+        private double inputValue, answer, runningTotal = 0;
 
         public Form1()
         {
             InitializeComponent();
 
-            // Hide the additional options and stop button
-            this.addMoreGB.Hide();
-            this.button2.Hide();
+            // Hide the reset button until initial input value added
+            this.resetBtn.Hide();
 
             // Set the cursor in the first textbox
-            this.firstValueText.Select();
+            this.inputValueTB.Select();
+
+            // Display the beginning total sum in the textbox
+            this.totalSumTB.Text = Convert.ToString(this.runningTotal);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                if (this.initialValuesGB.Visible)
+                // Try to convert from string to double value
+                this.inputValue = Convert.ToDouble(this.inputValueTB.Text);
+
+                // Calculate the answer, add to running total, and display the result
+                this.answer = this.inputValue + this.runningTotal;
+                this.runningTotal = this.answer;
+                this.totalSumTB.Text = Convert.ToString(this.answer);
+
+                // Unhide the reset button
+                this.resetBtn.Show();
+
+                // Reset the input textbox
+                this.inputValueTB.Text = "";
+
+                // Set the cursor in the input textbox
+                this.inputValueTB.Select();
+
+                //Output the running total and reset the accumulator if input value is 0
+                if(this.inputValue == 0)
                 {
-                    // Try to convert from string to double value
-                    this.value1 = Convert.ToDouble(this.firstValueText.Text);
-                    this.value2 = Convert.ToDouble(this.secondValueText.Text);
-
-                    // Calculate the answer, add to running total, and display the result
-                    this.answer = this.value1 + this.value2;
-                    this.runningTotal = this.answer;
-                    this.resultTextBox.Text = Convert.ToString(answer);
-
-                    // Unhide the running total objects
-                    this.addMoreGB.Show();
-                    this.button2.Show();
-
-                    // Hide the first and second value objects
-                    this.initialValuesGB.Hide();
-
-                    // Change the answer label to running total
-                    this.label4.Text = "Total";
-
-                    // Set the cursor in the textbox
-                    this.addMoreTB.Select();
-
-                }
-                else
-                {
-                    // Try to convert from string to double value
-                    this.value3 = Convert.ToDouble(this.addMoreTB.Text);
-
-                    // Calculate the answer, add to running total, and display the result
-                    this.answer = this.runningTotal + this.value3;
-                    this.runningTotal = this.answer;
-                    this.resultTextBox.Text = Convert.ToString(answer);
-
-                    // Reset the input textbox with the cursor in the textbox
-                    this.addMoreTB.Text = "";
-                    this.addMoreTB.Select();
+                    MessageBox.Show($" Total = {this.runningTotal}", "Running Total");
+                    button2_Click(sender, e);
                 }
 
             }
             catch
             {
                 // Catch the error received from bad input data and display it as an output
-                MessageBox.Show("Error on input", "WARNING");
+                MessageBox.Show("Error on input. Input requires a number.", "WARNING");
             }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Stops the running total and hides all running total objects
-            this.addMoreGB.Hide();
-            this.button2.Hide();
+            // Stops the running total and hides reset button
+            this.resetBtn.Hide();
 
-            // Resets the original text boxes
-            this.firstValueText.Text = "";
-            this.secondValueText.Text = "";
-            this.label4.Text = "Answer";
-            this.resultTextBox.Text = "";
+            // Resets the input and total sum textboxes
+            this.inputValueTB.Text = "";
+            this.totalSumTB.Text = "";
 
-            // Unhides the original text boxes and labels
-            this.initialValuesGB.Show();
+            // Reset the total sum
+            this.runningTotal = 0;
 
-            // Set the cursor in the first textbox
-            this.firstValueText.Select();
+            // Display the new total sum in the textbox
+            this.totalSumTB.Text = Convert.ToString(this.runningTotal);
+
+            // Set the cursor in the input textbox
+            this.inputValueTB.Select();
         }
     }
 }
